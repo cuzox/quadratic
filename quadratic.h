9eb1400f05d94992295b4ce0ed1a64449a221db3
smithaj22 <austin.smith@wmich.edu>
2015-11-02 10:51:54 -0500

    Create quadratic.h

---
+++ quadratic.h
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define BUFFERSIZE 30
+
+typedef struct {
+	float a;
+	float b;
+	float c;
+} COEFS;
+
+typedef struct {
+	char a[BUFFERSIZE];
+	char b[BUFFERSIZE];
+	char c[BUFFERSIZE];
+} ARGS;
+
+typedef struct{
+	float root[2];
+	int complex;
+	int equal;
+	int real;
+} ROOTS;
+
+void *getArgs(int, char*[]);
+void *getCoefs(ARGS*);
+void *solveQ(COEFS*);
+

99a56d1690a63db158ed0ae46b5e50605a8fbfd8
jameskwapien <james.m.kwapien@wmich.edu>
2015-11-09 12:05:19 -0500

    failed test as well

--- tests/tester.c
+++ tests/tester.c
@@ -8,7 +8,10 @@
 
 int main(int argc, char *argvp[]){
 
-	COEFS coefs; /* a, b and c for the quadratic equation */
+	cunit_init();
+
+	COEFS *coefs;
+	COEFS coefs2; /* a, b and c for the quadratic equation */
 	// ROOTS roots; /* root struct with x1 and x2 */
 	ROOTS *ret; /* return value from quadratic.c */
 	/* temporary variables */
@@ -34,16 +37,28 @@ int main(int argc, char *argvp[]){
 	assert_seq("ret", args->a, "5");
 	assert_seq("ret", args->b, "6");
 	assert_seq("ret", args->c, "1");
+	// free(a2);
 
 	/* getCoefs unit tests */
+	ARGS *args2 = malloc(sizeof(ARGS));
+	strncpy(args2->a, "5", BUFFERSIZE);
+	strncpy(args2->b, "6", BUFFERSIZE);
+	strncpy(args2->c, "1", BUFFERSIZE);
+	float a = 5.0;
+	float b = 6.0;
+	float c = 1.1;
+	coefs = getCoefs(args2); 
+	assert_feq("ret", a, coefs->a);
+	assert_feq("ret", b, coefs->b);
+	assert_feq("ret", c, coefs->c);
 
 	/* solveQ unit tests */
 	x1 = 3.1;
 	x2 = 3.3;
-	coefs.a = 1.0;
-	coefs.b = -x1 + -x2;
-	coefs.c = x1 * x2;
-	ret = solveQ(&coefs);
+	coefs2.a = 1.0;
+	coefs2.b = -x1 + -x2;
+	coefs2.c = x1 * x2;
+	ret = solveQ(&coefs2);
 	assert_eq("ret", ret->real, 1);
 	// assert_feqrerr("x1", ret->root[0], x2, 10.0 * cunit_dmacheps);
 
